cb695ddb413e4e55e305eaca38283b9c899c25f0
marvinestrada21 <marvinestrada2@gmail.com>
2017-05-19 16:26:46 -0600

    Agregar todas las obras

--- Main.cpp
+++ Main.cpp
@@ -21,7 +21,7 @@ int main(){
                 switch(menu()){
                         case 1:{
 				switch(menu2()){
-					case 1:{
+					case 1:{//Literatura
 						string genero, epoca;
 						cout << "Ingrese el género literario: " << endl;
 						cin >> genero;
@@ -38,36 +38,69 @@ int main(){
 						break;
 					};
 
-					case 2:{
+					case 2:{//Esculturas
 						string peso, material;
-						
+						cout << "Ingrese el peso: " << endl;
+						cin >> peso;
+						cout << "Ingrese el material con el que está hecho: " << endl;
+						cin >> material;
+						cout << "Ingrese el nombre: " << endl;
+						cin >> nombre;
+						cout << "Ingrese el artista: " << endl;
+						cin >> artista;
+						cout << "Ingrese la fecha de ingreso: " << endl;
+						cin >> fecha;
+						Esculturas esculturas(peso, material, id, nombre, artista, fecha);
+						olist.push_back(esculturas);
 						break;
 					};
 
-					case 3:{
+					case 3:{//Pinturas
 						string material_lienzo, tecnica;
+						cout << "Ingrese el material del lienzo: " << endl;
+						cin >> material_lienzo;
+						cout << "Ingrese la técnica que se utilizó: " << endl;
+						cin >> tecnica;
+						cout << "Ingrese el nombre: " << endl;
+						cin >> nombre;
+						cout << "Ingrese el artista: " << endl;
+						cin >> artista;
+						cout << "Ingrese la fecha de ingreso: " << endl;
+						cin >> fecha;
+						Pinturas pinturas(material_lienzo, tecnica, id, nombre, artista, fecha);
+						olist.push_back(pinturas);
 						break;
 					};
 
-					case 4:{
+					case 4:{//Diseños
 						string terreno;
+						cout << "Ingrese el terreno en donde está diseñada: " << endl;
+						cin >> terreno;
+						cout << "Ingrese el nombre: " << endl;
+						cin >> nombre;
+						cout << "Ingrese el artista: " << endl;
+						cin >> artista;
+						cout << "Ingrese la fecha de ingreso: " << endl;
+						cin >> fecha;
+						Disenos disenos(terreno, id, nombre, artista, fecha);
+						olist.push_back(disenos);
 						break;
 					};
-				}
+				}//Fin switch
 				break;
 			};
 			
-			case 2:{
+			case 2:{//Eliminar
 			
 				break;
 			};
 			
-			case 3:{
+			case 3:{//Transferir Obras
 			
 				break;
 			};
 
-			case 4:{
+			case 4:{//Reportes
 			
 				break;
 			};
@@ -76,8 +109,8 @@ int main(){
                                 salir = true;
                                 break;
 			};
-		}
-	}
+		}//Fin switch
+	}//Fin while
 	return 0;
 }
 
